728cec2bfee78058aca98a286744fc6416b138bd
baxtiyor01 <baxtiyorrajabboyev013@gmail.com>
2025-11-26 17:53:29 +0500

    init

---
+++ backend1/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      username: "postgres",
+      host: "localhost",
+      port: 5432,
+      database: "microservice",
+      password: "1111",
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    ProductModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ backend1/src/product/entities/product.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "product" })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+}
